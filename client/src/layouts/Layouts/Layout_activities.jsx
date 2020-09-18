959b62ae6684bb00703bcea7c443b9692a8fc0cb
donnywarren9 <donnywarren9@gmail.com>
2020-09-18 10:49:38 -0400

    layouts files created

---
+++ client/src/layouts/Layouts/Layout_activities.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Layout_activities.css";
+
+export default function LayoutActivities() {
+  return (
+    <>
+      <div>ACT</div>
+    </>
+  );
+}
---
+++ client/src/layouts/Layouts/Layout_welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Layout_welcome.css";
+
+export default function LayoutWelcome() {
+  return (
+    <>
+      <div>ltkj</div>
+    </>
+  );
+}
